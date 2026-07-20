7b893685a22ab3e663b88437fdac55ff975bd8f3
Felix-au <felixaugum@gmail.com>
2026-07-20 14:00:00 +0530

    feat(components): setup hero slide presentation banner

---
+++ src/components/HeroSlide.jsx
@@ -0,0 +1,13 @@
+import CURSORS from '../data/cursors.js';
+
+const ACCENT_COLORS = [
+  '#7c5cfc', '#5cf4fc', '#fc5cb8', '#5cfca8', '#fca85c',
+  '#fc5c5c', '#c45cfc', '#5c8afc', '#fcf25c', '#5cfcdc',
+  '#fc8a5c', '#5cfc7c', '#fc5c8a', '#5cb8fc', '#d4fc5c',
+  '#fc5cd4', '#5cfcf4', '#fc9c5c', '#5c5cfc', '#fc5c9c',
+  '#8afc5c', '#5cfca0', '#fcb85c', '#a05cfc', '#fc5c68',
+];
+
+export default function HeroSlide({ onNavigate }) {
+  return null;
+}
