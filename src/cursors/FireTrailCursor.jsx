026669b74b99ec9603c545de0cc7afd10abfdf8f
Felix-au <felixaugum@gmail.com>
2026-07-22 17:39:00 +0530

    feat(cursor): raise fire trail canvas z-index layer

--- src/cursors/FireTrailCursor.jsx
+++ src/cursors/FireTrailCursor.jsx
@@ -61,5 +61,5 @@ export default function FireTrailCursor({ containerRef, config }) {
     return () => { container.removeEventListener('mousemove', onMove); ro.disconnect(); cancelAnimationFrame(raf); };
   }, [containerRef]);
 
-  return <canvas ref={canvasRef} style={{ position: 'absolute', inset: 0, pointerEvents: 'none', zIndex: 5 }} />;
+  return <canvas ref={canvasRef} style={{ position: 'absolute', inset: 0, pointerEvents: 'none', zIndex: 35 }} />;
 }
