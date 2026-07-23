b4e54fd0b6d90d591a3d579288e04cca2930b324
Felix-au <felixaugum@gmail.com>
2026-07-23 21:20:33 +0530

    style(bg): refine Cubes background size to 60px and gap to 28px

--- src/App.jsx
+++ src/App.jsx
@@ -334,8 +334,8 @@ export default function App() {
       <Cubes
         rows={7}
         cols={14}
-        cubeSize={63}
-        cellGap={25}
+        cubeSize={60}
+        cellGap={28}
         maxAngle={90}
         radius={3}
         borderStyle="1px dashed #5cf4fc"
